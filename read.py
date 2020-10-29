249f9eaca6012d192d7bc363f5a560674d9aeb84
Kim Brandt-Madsen <bea1209@gmail.com>
2020-10-29 10:14:49 +0100

    calc avg read.py

--- read.py
+++ read.py
@@ -2,12 +2,15 @@ data = []
 count = 0
 with open ('reviews.txt', 'r') as f:
     for line in f:
-    	data.append(line)
-    	count += 1
-    	if count % 1000 == 0:
-    	print(len(data))
-print(len(data))
+        data.append(line)
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
+print('Files finished reading, we have total', len(data), 'reviews')
 
-print(data[0])
-print('---------------')
-print(data[1])
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+
+print('The average number of the reviews', sum_len/len(data)) 
+
