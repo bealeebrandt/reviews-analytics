46e4448c3f0a6964a025f3928b2e5f83b0cd5bb7
Kim Brandt-Madsen <bea1209@gmail.com>
2020-10-29 11:00:27 +0100

    filtering comment read2.py

---
+++ read2.py
@@ -0,0 +1,33 @@
+data = []
+count = 0
+with open ('reviews.txt', 'r') as f:
+    for line in f:
+        data.append(line)
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
+print('Files finished reading, we have total', len(data),'reviews')
+
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+
+print('The average number of the reviews', sum_len/len(data)) 
+
+new = []
+for d in data:
+    if len(d) < 100:
+    	new.append(d)
+print('We have total', len(new),'reviews less than 100 words!')
+print(new[0])
+print('-----------------')
+print(new[1])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('total number of reviews', len(good), 'mentioned good in the content')
+print(good[0])
+print('-----------------')
+print(good[1])
